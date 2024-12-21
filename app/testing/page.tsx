b7406f12bc0e08ac00a5ecd1ed85e6d0b9a134c3
JordanReady <jready112@gmail.com>
2024-12-21 15:48:28 -0600

    fix: deployment

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -5,14 +5,6 @@ export default function TestingPage() {
   return (
     <div className="testing animate">
       <h2>Testing</h2>
-      <PlayerInformation
-        playerImgScale="1.5"
-        pos={1}
-        animate="true"
-        imgSrc="/OTMSLogo.png"
-        playerImgSrc="/Chippy.png"
-        aspectRatio="Banner"
-      />
     </div>
   );
 }
